2f5d2d6573c3050ee2069685c573ae9bed53cf57
emdant <30555419+emdant@users.noreply.github.com>
2025-06-15 20:49:08 +0100

    Refactored padded array

--- include/parallel/padded_array.h
+++ include/parallel/padded_array.h
@@ -5,6 +5,7 @@
 #define PARALLEL_PADDED_ARRAY_H_
 
 #include <algorithm>
+#include <cstddef>
 
 #include "alignment.h"
 
@@ -13,31 +14,25 @@ namespace parallel {
 template <typename T>
 class padded_array {
 public:
-  static inline constexpr size_t stride() {
-    if constexpr (sizeof(T) > hardware_destructive_interference_size) {
-      return 1 + (sizeof(T) % hardware_destructive_interference_size != 0 ? 1 : 0);
-    } else {
-      constexpr int trunc = hardware_destructive_interference_size / sizeof(T);
-      constexpr float f = hardware_destructive_interference_size / sizeof(T);
-      return f > trunc ? trunc + 1 : trunc;
-    }
-  }
+  struct alignas(hardware_destructive_interference_size) padded_element {
+    T element;
+  };
 
   struct iterator {
     using iterator_category = std::forward_iterator_tag;
     using difference_type = std::ptrdiff_t;
-    using value_type = T;
+    using value_type = padded_element;
     using pointer = value_type*;
-    using reference = value_type&;
+    using reference = T&;
 
-    inline iterator(pointer ptr) : m_ptr(ptr) {}
+    inline iterator(pointer ptr) : ptr_(ptr) {}
 
-    inline reference operator*() const { return *m_ptr; }
-    inline pointer operator->() { return m_ptr; }
+    inline reference operator*() const { return ptr_->element; }
+    inline pointer operator->() { return ptr_; }
 
     // Prefix increment
     inline iterator& operator++() {
-      m_ptr += stride();
+      ptr_++;
       return *this;
     }
 
@@ -48,18 +43,24 @@ public:
       return tmp;
     }
 
-    inline friend bool operator==(const iterator& a, const iterator& b) { return a.m_ptr == b.m_ptr; };
-    inline friend bool operator!=(const iterator& a, const iterator& b) { return a.m_ptr != b.m_ptr; };
+    inline friend bool operator==(const iterator& a, const iterator& b) { return a.ptr_ == b.ptr_; };
+    inline friend bool operator!=(const iterator& a, const iterator& b) { return a.ptr_ != b.ptr_; };
+    inline difference_type operator-(const iterator& other) const {
+      return ptr_ - other.ptr_;
+    }
+    inline reference operator[](difference_type n) const {
+      return (ptr_ + n)->element;
+    }
 
   private:
-    pointer m_ptr;
+    pointer ptr_;
   };
 
   padded_array() : start_(nullptr), end_size_(nullptr) {}
 
   explicit padded_array(size_t num_elements) {
-    start_ = new T[num_elements * stride()];
-    end_size_ = start_ + (num_elements * stride());
+    start_ = new padded_element[num_elements];
+    end_size_ = start_ + num_elements;
   }
 
   padded_array(size_t num_elements, const T& init_val) : padded_array(num_elements) {
@@ -70,11 +71,12 @@ public:
       : padded_array(copy_end - copy_begin) {
 #pragma omp parallel for schedule(static, 1)
     for (size_t i = 0; i < size(); i++)
-      start_[i * stride()] = copy_begin[i * stride()];
+      start_[i].element = copy_begin[i];
   }
 
   // don't want this to be copied, too much data to move
   padded_array(const padded_array& other) = delete;
+  padded_array& operator=(const padded_array& other) = delete;
 
   // prefer move because too much data to copy
   padded_array(padded_array&& other)
@@ -106,32 +108,32 @@ public:
   }
 
   T& operator[](size_t n) {
-    return start_[n * stride()];
+    return start_[n].element;
   }
 
   const T& operator[](size_t n) const {
-    return start_[n * stride()];
+    return start_[n].element;
   }
 
   void fill(T init_val) {
 #pragma omp parallel for schedule(static, 1)
     for (size_t i = 0; i < size(); i++)
-      start_[i * stride()] = init_val;
+      start_[i].element = init_val;
   }
 
   size_t size() const {
-    return (end_size_ - start_) / stride();
+    return end_size_ - start_;
   }
 
-  iterator begin() const {
+  const iterator begin() const {
     return iterator(start_);
   }
 
-  iterator end() const {
+  const iterator end() const {
     return iterator(end_size_);
   }
 
-  T* data() const {
+  padded_element* data() const {
     return start_;
   }
 
@@ -141,8 +143,8 @@ public:
   }
 
 private:
-  T* start_;
-  T* end_size_;
+  padded_element* start_;
+  padded_element* end_size_;
 };
 
 } // namespace parallel
